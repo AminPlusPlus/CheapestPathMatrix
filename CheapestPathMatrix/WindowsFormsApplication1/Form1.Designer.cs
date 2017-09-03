dccfb25d840c0cb15267c83b7f89bdefa3c07dac
Aminjoni Abdullozoda <aamingames@gmail.com>
2017-09-03 15:26:23 +0200

    Release Project

---
+++ CheapestPathMatrix/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.locationName = new System.Windows.Forms.Label();
+            this.Creator = new System.Windows.Forms.Label();
+            this.StartIndex = new System.Windows.Forms.TextBox();
+            this.startlbl = new System.Windows.Forms.Label();
+            this.endlbl = new System.Windows.Forms.Label();
+            this.EndIndex = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richMatrix = new System.Windows.Forms.RichTextBox();
+            this.richGraph = new System.Windows.Forms.RichTextBox();
+            this.total = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.title.Location = new System.Drawing.Point(156, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(274, 31);
+            this.title.TabIndex = 1;
+            this.title.Text = "Cheapest Path Matrix";
+            // 
+            // locationName
+            // 
+            this.locationName.AutoSize = true;
+            this.locationName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.locationName.Location = new System.Drawing.Point(232, 73);
+            this.locationName.Name = "locationName";
+            this.locationName.Size = new System.Drawing.Size(54, 13);
+            this.locationName.TabIndex = 2;
+            this.locationName.Text = "Name File";
+            // 
+            // Creator
+            // 
+            this.Creator.AutoSize = true;
+            this.Creator.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Creator.Location = new System.Drawing.Point(1, 459);
+            this.Creator.Name = "Creator";
+            this.Creator.Size = new System.Drawing.Size(266, 16);
+            this.Creator.TabIndex = 3;
+            this.Creator.Text = "All rights reserved by Aminjoni Abdullozoda";
+            // 
+            // StartIndex
+            // 
+            this.StartIndex.Location = new System.Drawing.Point(235, 113);
+            this.StartIndex.Multiline = true;
+            this.StartIndex.Name = "StartIndex";
+            this.StartIndex.Size = new System.Drawing.Size(114, 22);
+            this.StartIndex.TabIndex = 4;
+            // 
+            // startlbl
+            // 
+            this.startlbl.AutoSize = true;
+            this.startlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startlbl.Location = new System.Drawing.Point(113, 115);
+            this.startlbl.Name = "startlbl";
+            this.startlbl.Size = new System.Drawing.Size(87, 20);
+            this.startlbl.TabIndex = 5;
+            this.startlbl.Text = "Start Index";
+            // 
+            // endlbl
+            // 
+            this.endlbl.AutoSize = true;
+            this.endlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.endlbl.Location = new System.Drawing.Point(113, 152);
+            this.endlbl.Name = "endlbl";
+            this.endlbl.Size = new System.Drawing.Size(81, 20);
+            this.endlbl.TabIndex = 7;
+            this.endlbl.Text = "End Index";
+            // 
+            // EndIndex
+            // 
+            this.EndIndex.Location = new System.Drawing.Point(235, 150);
+            this.EndIndex.Multiline = true;
+            this.EndIndex.Name = "EndIndex";
+            this.EndIndex.Size = new System.Drawing.Size(114, 22);
+            this.EndIndex.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(24, 193);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(243, 214);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richMatrix);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(235, 188);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "The Matrix Is";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // richMatrix
+            // 
+            this.richMatrix.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richMatrix.Location = new System.Drawing.Point(6, 6);
+            this.richMatrix.Name = "richMatrix";
+            this.richMatrix.ReadOnly = true;
+            this.richMatrix.Size = new System.Drawing.Size(221, 176);
+            this.richMatrix.TabIndex = 0;
+            this.richMatrix.Text = "";
+            // 
+            // richGraph
+            // 
+            this.richGraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richGraph.Location = new System.Drawing.Point(6, 3);
+            this.richGraph.Name = "richGraph";
+            this.richGraph.ReadOnly = true;
+            this.richGraph.Size = new System.Drawing.Size(263, 179);
+            this.richGraph.TabIndex = 0;
+            this.richGraph.Text = "";
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.total.Location = new System.Drawing.Point(288, 425);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(90, 25);
+            this.total.TabIndex = 10;
+            this.total.Text = "Total Is";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(369, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 59);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Show Result";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage2);
+            this.tabControl2.Location = new System.Drawing.Point(273, 193);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(283, 214);
+            this.tabControl2.TabIndex = 12;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richGraph);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(275, 188);
+            this.tabPage2.TabIndex = 0;
+            this.tabPage2.Text = "The Cheapest Path is";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(564, 484);
+            this.Controls.Add(this.tabControl2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.endlbl);
+            this.Controls.Add(this.EndIndex);
+            this.Controls.Add(this.startlbl);
+            this.Controls.Add(this.StartIndex);
+            this.Controls.Add(this.Creator);
+            this.Controls.Add(this.locationName);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Cheapest Path Matrix";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label locationName;
+        private System.Windows.Forms.Label Creator;
+        private System.Windows.Forms.TextBox StartIndex;
+        private System.Windows.Forms.Label startlbl;
+        private System.Windows.Forms.Label endlbl;
+        private System.Windows.Forms.TextBox EndIndex;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richMatrix;
+        private System.Windows.Forms.RichTextBox richGraph;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ CheapestPathMatrix/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        static float[,] matrix;
+        static int m, n;
+        static int endX, endY;
+
+        public string fileName { get; set; }
+        public int cleatTexts { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            cleatTexts = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "txt File |*.txt";
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = fd.FileName;
+                    locationName.Text = fileName;
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cleatTexts++;
+            if (cleatTexts > 1)
+            {
+                richMatrix.Clear();
+                richGraph.Clear();
+            }
+            try
+            {
+                // Giving Value for Start Index
+                string startIndexString = StartIndex.Text;
+                string[] words = startIndexString.Split(',');
+                int startX = int.Parse(words[0]);
+                int startY = int.Parse(words[1]);
+
+                //Giving Value for End Index
+                string endIndexString = EndIndex.Text;
+                string[] words2 = endIndexString.Split(',');
+                endX = int.Parse(words2[0]);
+                endY = int.Parse(words2[1]);
+
+
+
+                // Reading Matrix from TXT file
+                string content = File.ReadAllText(fileName);
+
+                string[] listarray = content.Split(';');
+
+                m = listarray.Length - 1;
+                n = listarray[0].Split(' ').Length;
+
+                matrix = new float[m, n];
+
+
+                for (int i = 0; i < m; i++)
+                {
+                    string[] list = listarray[i].Trim().Split();
+                    int l = list.Length;
+                    for (int j = 0; j < l; j++)
+                    {
+                        matrix[i, j] = float.Parse(list[j]);
+                    }
+                }
+
+                // Showing Matrix
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        richMatrix.AppendText(" " + matrix[i, j]);
+                    }
+                    richMatrix.Text += Environment.NewLine + "";
+                }
+
+                int[,] register = make_zero_register_matrix(m, n);
+                position end = move(startX, startY, register);
+
+                // Cheapest Path
+                for (int k = 0; k < m; k++)
+                {
+                    for (int l = 0; l < n; l++)
+                    {
+                        if (end.register[k, l] == 0) richGraph.AppendText("- ");
+                        else richGraph.AppendText("+ ");
+                    }
+                    richGraph.Text += Environment.NewLine + "";
+                }
+
+                double result = end.subtotal - end.last_item - matrix[0, 0];    // Here is showing Result of Total Matrix
+                // total
+                total.Text = "Total is : " + result;
+            }
+            catch (ArgumentNullException e1)
+            {
+                MessageBox.Show("Please choose Matrix file ");
+            }
+            catch (FormatException e2)
+            {
+                MessageBox.Show("Please write START INDEX MATRIX and END INDEX MATRIX");
+            }
+            catch (IndexOutOfRangeException e3)
+            {
+                MessageBox.Show("Please write START INDEX MATRIX & END INDEX MATRIX Correctly");
+            }
+            catch
+            {
+                MessageBox.Show("Please write START INDEX MATRIX & END INDEX MATRIX Correctly");
+            }
+
+
+        }
+
+
+        public static int[,] make_zero_register_matrix(int i, int j)
+        {
+            int[,] register = new int[i, j];
+            for (int k = 0; k < i; k++)
+            {
+                for (int l = 0; l < j; l++)
+                {
+                    register[k, l] = 0;
+                }
+            }
+            return register;
+        }
+
+        static position move(int i, int j, int[,] register)
+        {
+            position pos = null;
+            int[,] reg;
+            position[] movement;
+            int movNumber;
+
+            if (register[i - 1, j - 1] == 1) return null;
+            if (i == endX && j == endY)
+            {
+                pos = new position();
+                pos.subtotal = matrix[i - 1, j - 1];
+                pos.register = make_zero_register_matrix(m, n);
+                pos.register[i - 1, j - 1] = 1;
+                pos.last_item = pos.subtotal;
+                return pos;
+            }
+
+
+            reg = new int[m, n];
+            Array.Copy(register, 0, reg, 0, register.Length);
+            reg[i - 1, j - 1] = 1;
+
+            if (i == 1 && j == 1)
+            {
+                movement = new position[3];
+                movement[0] = move(i, j + 1, reg);
+                movement[1] = move(i + 1, j + 1, reg);
+                movement[2] = move(i + 1, j, reg);
+                movNumber = 3;
+            }
+            else
+                if (i == 1 && j == n)
+                {
+                    movement = new position[3];
+                    movNumber = 3;
+                    movement[0] = move(i + 1, j, reg);
+                    movement[1] = move(i + 1, j - 1, reg);
+                    movement[2] = move(i, j - 1, reg);
+                }
+                else
+                    if (i == m && j == n)
+                    {
+                        movement = new position[3];
+                        movement[0] = move(i, j - 1, reg);
+                        movement[1] = move(i - 1, j - 1, reg);
+                        movement[2] = move(i - 1, j, reg);
+                        movNumber = 3;
+                    }
+                    else
+                        if (i == m && j == 1)
+                        {
+                            movement = new position[3];
+                            movNumber = 3;
+                            movement[0] = move(i, j + 1, reg);
+                            movement[1] = move(i - 1, j + 1, reg);
+                            movement[2] = move(i - 1, j, reg);
+                        }
+                        else
+                            if (i == 1)
+                            {
+                                movement = new position[5];
+                                movement[0] = move(i, j + 1, reg);
+                                movement[1] = move(i + 1, j + 1, reg);
+                                movement[2] = move(i + 1, j, reg);
+                                movement[3] = move(i + 1, j - 1, reg);
+                                movement[4] = move(i, j - 1, reg);
+                                movNumber = 5;
+                            }
+                            else
+                                if (i == m)
+                                {
+                                    movement = new position[5];
+                                    movement[0] = move(i - 1, j - 1, reg);
+                                    movement[1] = move(i - 1, j, reg);
+                                    movement[2] = move(i - 1, j + 1, reg);
+                                    movement[3] = move(i, j + 1, reg);
+                                    movement[4] = move(i, j - 1, reg);
+                                    movNumber = 5;
+                                }
+                                else
+                                    if (j == 1)
+                                    {
+                                        movement = new position[5];
+                                        movement[0] = move(i - 1, j, reg);
+                                        movement[1] = move(i - 1, j + 1, reg);
+                                        movement[2] = move(i, j + 1, reg);
+                                        movement[3] = move(i + 1, j + 1, reg);
+                                        movement[4] = move(i + 1, j, reg);
+                                        movNumber = 5;
+                                    }
+                                    else
+                                        if (j == n)
+                                        {
+                                            movement = new position[5];
+                                            movement[0] = move(i - 1, j - 1, reg);
+                                            movement[1] = move(i - 1, j, reg);
+                                            movement[2] = move(i + 1, j, reg);
+                                            movement[3] = move(i + 1, j - 1, reg);
+                                            movement[4] = move(i, j - 1, reg);
+                                            movNumber = 5;
+                                        }
+                                        else
+                                        {
+                                            movement = new position[8];
+                                            movement[0] = move(i - 1, j - 1, reg);
+                                            movement[1] = move(i - 1, j, reg);
+                                            movement[2] = move(i - 1, j + 1, reg);
+                                            movement[3] = move(i, j + 1, reg);
+                                            movement[4] = move(i + 1, j + 1, reg);
+                                            movement[5] = move(i + 1, j, reg);
+                                            movement[6] = move(i + 1, j - 1, reg);
+                                            movement[7] = move(i, j - 1, reg);
+                                            movNumber = 8;
+                                        }
+
+
+            for (int k = 0; k < movNumber; k++)
+            {
+                if (movement[k] != null)
+                {
+                    if (pos == null)
+                    {
+                        pos = new position();
+                        pos = movement[k];
+                    }
+                    else
+                    {
+                        if (pos.subtotal > movement[k].subtotal)
+                            pos = movement[k];
+                    }
+                }
+            }
+
+            if (pos != null)
+            {
+                pos.subtotal += matrix[i - 1, j - 1];
+                pos.register[i - 1, j - 1] = 1;
+            }
+            return pos;
+        }
+        
+
+      
+    }
+
+    class position
+    {
+        public int[,] register;
+        public float subtotal = 0;
+        public float last_item;
+    }  
+}
